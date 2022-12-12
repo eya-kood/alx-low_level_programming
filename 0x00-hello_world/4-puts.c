00e77ed1d9d2f43ce42712600fa42c0152a17f0e
eyassu <eyukassa18@gmail.com>
2022-12-12 04:49:19 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
-#include <stdio.h>
-/**
- * main- main block
- * return 0
+#include<stdio.h>
+/*
+ * main -main block
+ * return: 0
  */
-int main(void) {
-
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
-
+int main(void){
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
